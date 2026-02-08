2910da6c2a6a16d7e6ce581da5ded1fd7fb164e6
Pellegrino Piccolo <piccolopellegrino22@gmail.com>
2026-02-08 06:53:04 +0100

    add title bar height variable and adjust main content padding

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import SnippetProvider from './context/SnippetContext'
 import { SearchProvider } from './context/SearchContext'
 import { ModalProvider } from './context/ModalContext'
 import TitleBar from './components/ui/TitleBar'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,7 +13,9 @@ createRoot(document.getElementById('root')!).render(
       <SearchProvider>
         <ModalProvider>
           {window.electronAPI.platform !== 'darwin' && <TitleBar />}
-          <App />
+          <div className={`${window.electronAPI.platform !== 'darwin' ? 'main-content' : ''}`}>
+            <App />
+          </div>
         </ModalProvider>
       </SearchProvider>
     </SnippetProvider>
